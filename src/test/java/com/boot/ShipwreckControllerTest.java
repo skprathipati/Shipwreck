d829270bb1087c05d5c1097ea01002adc416ef67
skprathipati <skumar.prathipati@gmail.com>
2018-02-04 08:18:18 -0500

    master
    first commit

---
+++ src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,44 @@
+package com.boot;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+
+public class ShipwreckControllerTest {
+	
+	@InjectMocks
+	private ShipwreckController sc;
+	
+	@Mock
+	private ShipwreckRepository shipwreckRepository;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testGetShipwreck() {
+		Shipwreck sw = new Shipwreck();
+		sw.setId(1L);
+		
+		when(shipwreckRepository.findOne(1L)).thenReturn(sw);
+		Shipwreck wreck = sc.get(1L);
+		
+		verify(shipwreckRepository).findOne(1L);
+		//assertEquals(1L, wreck.getId().longValue());
+		assertThat(wreck.getId(), is(1L));
+	}
+}
